ab96e68a512406cd91f327427aa338bf10dab950
rafsan007 <rafsanahmad111@gmail.com>
2017-11-30 19:28:56 +0600

    Code Pushed

---
+++ app/src/main/java/com/rafsan/recyclerviewsearch/Contact.java
@@ -0,0 +1,22 @@
+package com.rafsan.recyclerviewsearch;
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
